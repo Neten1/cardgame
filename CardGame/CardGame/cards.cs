37cc2ab3c617009a2f2a38a0b0ae72f194de03b1
Tymon Maciejewski <68917880+Neten1@users.noreply.github.com>
2024-03-29 22:25:54 +0100

    Add files via upload
    Version 0.1

---
+++ CardGame/CardGame/cards.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    public enum Origin
+    {
+        None,
+        OldCamp,
+        NewCamp,
+        SectCamp,
+        Monster,
+        Undead
+    }
+    public abstract class Card
+    {
+        protected int health, cost;
+        public int currentHealth { get; private set; }
+        public int damage { get; private set; }
+        public string name { get; private set; }
+        public Origin origin { get; private set; }
+        public Card(string name, int health, int damage, int cost, Origin origin)
+        {
+            this.health = health;
+            currentHealth = health;
+            this.damage = damage;
+            this.cost = cost;
+            this.name = name;
+            this.origin = origin;
+        }
+        public virtual void Info()
+        {
+            Console.WriteLine($"{name} ({cost}) - HP {health} || DMG {damage} || {origin}");
+        }
+        public virtual void Status() //returns current information about the card
+        {
+            Console.WriteLine($"{name} : HP {currentHealth}/{health}|| DMG {damage}");
+        }
+        public virtual string StatusString()
+        {
+            return $"{name} : {currentHealth}/{health} || {damage}";
+        }
+        protected void ChangeHealth(int dmg)
+        {
+            currentHealth -= dmg;
+        }
+        public virtual void Attack(Card defender)
+        {
+            defender.Defend(this);
+            Console.WriteLine($"{this.name} attacked {defender.name}!");
+            if (defender.origin == origin && origin != Origin.None)
+            {
+                ChangeHealth(defender.damage - 1);
+            }
+            else { ChangeHealth(defender.damage); }
+        }
+        public virtual void Defend(Card attacker)
+        {
+            ChangeHealth(attacker.damage);
+        }
+    }
+    public class StandardCard : Card
+    {
+        public StandardCard(string name, int health, int damage, int cost, Origin origin) : base(name, health, damage, cost, origin)
+        {
+        }
+    }
+    public class TauntCard : Card
+    {
+        public TauntCard(string name, int health, int damage, int cost, Origin origin) : base(name, health, damage, cost, origin)
+        {
+        }
+        public override void Info()
+        {
+            Console.WriteLine($"{name} ({cost}) : HP {health} || DMG {damage} || {origin} || #TAUNT#");
+        }
+        public override string StatusString()
+        {
+            return $"#TAUNT# {name} : HP {currentHealth}/{health} || DMG {damage}";
+        }
+        public override void Status() //returns current information about the card
+        {
+            Console.WriteLine($"#TAUNT# {name} : HP {currentHealth}/{health} || DMG {damage}");
+        }
+        public override void Defend(Card attacker)
+        {
+            if (attacker.origin == origin && origin != Origin.None)
+            {
+                ChangeHealth(attacker.damage - 1);
+            }
+            else { ChangeHealth(attacker.damage); }
+        }
+    }
+    public class PoisonCard : Card
+    {
+        public PoisonCard(string name, int health, int damage, int cost, Origin origin) : base(name, health, damage, cost, origin)
+        {
+        }
+        public override void Info()
+        {
+            Console.WriteLine($"{name} ({cost}) : HP {health} || DMG {damage} || {origin} || <<LETHAL>>");
+        }
+        public override string StatusString()
+        {
+            return $"!LETHAL! {name} : HP {currentHealth}/{health} || DMG {damage}";
+        }
+        public override void Status() //returns current information about the card
+        {
+            Console.WriteLine($"!LETHAL! {name} : HP  {currentHealth} / {health}  || DMG {damage}");
+        }
+        public override void Attack(Card defender)
+        {
+            StandardCard tmp = new StandardCard("one-shot", 1, 999, 0, Origin.None);
+            defender.Defend(tmp);
+            if (defender.origin == origin)
+            {
+                ChangeHealth(defender.damage - 1);
+            }
+            else { ChangeHealth(defender.damage); }
+        }
+    }
+}
---
+++ CardGame/CardGame/player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class player
+    {
+        public string name { get; private set; }
+        private List<Card> deck;
+        public Card[] board { get; private set; }
+        public player(string name, List<Card> deck)
+        {
+            this.name = name;
+            this.deck = deck;
+            board = new Card[5];
+        }
+        public void AddFromDeckToBoard()
+        {
+            Console.WriteLine($"Pick which card should be placed on the board. (1-{deck.Count})");
+            ShowDeck();
+            int card_index_choice = Int32.Parse(Console.ReadLine()) - 1;
+            Console.WriteLine($"Pick where should the card be placed. (1-5)");
+            int card_placement_choice = Int32.Parse(Console.ReadLine()) - 1;
+            AddToBoard(deck[card_index_choice], card_placement_choice);
+        }
+        private void AddToBoard(Card card, int position)
+        {
+            board[position] = card;
+            deck.Remove(card);
+        }
+        public void DeleteCardFromBoard(int index)
+        {
+            board[index] = null;
+        }
+        public void ShowDeck()
+        {
+            Console.WriteLine($"{name}'s deck");
+            int count = 1;
+            foreach(Card i in deck)
+            {
+                Console.Write($"{count} : ");
+                i.Info();
+                count++;
+            }
+        }
+        public void ShowBoard()
+        {
+            Console.WriteLine($"{name}'s current board");
+            foreach (Card i in board)
+            {
+                if (i == null) Console.WriteLine("[/]");
+                else i.Status();
+            }
+        }
+        public Card WhoShouldAttack()
+        {
+            Console.WriteLine("Pick your attacker (1-5) :");
+            int choice = Int32.Parse(Console.ReadLine()) - 1;
+            return board[choice];
+        }
+        private bool hasTaunt(Card[] b)
+        {
+            foreach(Card i in b)
+            {
+                if (i is TauntCard) return true;
+            }return false;
+        }
+        public Card WhatToAttack(player enemy)
+        {
+            bool taunt = hasTaunt(enemy.board);
+            Console.WriteLine("Pick who to attack (1-5)");
+            for(int i = 0; i < 5; i++)
+            {
+                if((taunt && enemy.board[i] is TauntCard) || (!taunt && enemy.board[i] is not null))
+                {
+                    Console.WriteLine($"{i+1} - {enemy.board[i].StatusString()}");
+                }
+            }
+            int choice = Int32.Parse(Console.ReadLine()) - 1;
+            return enemy.board[choice];
+        }
+    }
+}
